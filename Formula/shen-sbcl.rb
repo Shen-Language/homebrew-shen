079f59e6397304cddca3f10c786276e2b0cfd5fc
Bruno Deferrari <utizoc@gmail.com>
2018-07-07 15:25:39 -0300

    Upgrade Shen/SBCL to 2.3.0

--- Formula/shen-sbcl.rb
+++ Formula/shen-sbcl.rb
@@ -1,14 +1,14 @@
 class ShenSbcl < Formula
   desc "Shen for Steel Bank Common Lisp"
   homepage "https://github.com/Shen-Language/shen-cl"
-  url "https://github.com/Shen-Language/shen-cl/archive/v2.2.0.tar.gz"
-  sha256 "bac94dd2a7918550c9c5c9e8970564d3a5d7beec7e10cc095d4d45c2eda034c3"
+  url "https://github.com/Shen-Language/shen-cl/archive/v2.3.0.tar.gz"
+  sha256 "feca83a3a72b8a0f81fbaa15f22c65a927140f83a1e86179aabd2352c10a334e"
 
   depends_on "sbcl" => :build
 
   resource "shen-kernel" do
-    url "https://github.com/Shen-Language/shen-sources/releases/download/shen-20.1/ShenOSKernel-20.1.tar.gz"
-    sha256 "c0a36109ab6d3ecbf86d09cd31e6e1e93ec010cea516c982a878c5808692f213"
+    url "https://github.com/Shen-Language/shen-sources/releases/download/shen-21.0/ShenOSKernel-21.0.tar.gz"
+    sha256 "7034fedf3513f8080c2a871e2d99689012fe72b59452e5dd87c80720b47409d4"
   end
 
   def install
